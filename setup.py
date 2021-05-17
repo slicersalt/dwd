ba8b1f63a3110e474200bfdb59b93b35b05fe7d8
David Allemang <david.allemang@kitware.com>
2021-05-17 16:05:09 -0400

    ENH: Add infrastructure for PyPi
    Also modify `setup.py`, `README.rst`, `__init__.py` to reflect that Kitware are the current maintainers for the project, but Iain is the author.
    
    Adds import aliases to `__init__.py` so that classifiers are accessible through `import dwd`

--- setup.py
+++ setup.py
@@ -1,19 +1,24 @@
 from setuptools import setup, find_packages
 
-# def readme():
-#     with open('README.rst') as f:
-#            return f.read()
-
 install_requires = ['numpy', 'cvxpy', 'sklearn', 'matplotlib']
 
-setup(name='dwd',
-      version='0.0.1',
-      description='Distance weighted discrimination.',
+setup(
+      name='dwd',
+      version='1.0.0',
       author='Iain Carmichael',
       author_email='idc9@cornell.edu',
+      maintainer='Kitware Medical',
+      maintainer_email='david.allemang@kitware.com',
       license='MIT',
+      description='Distance weighted discrimination for Python',
+      long_description=(
+            'This package implements Distance Weighted Discrimination (DWD). DWD For '
+            'details see (Marron et al 2007, Wang and Zou 2018). Originally implemented '
+            'in Python by Iain Carmichael.'
+      ),
       packages=find_packages(),
       install_requires=install_requires,
       test_suite='nose.collector',
       tests_require=['nose'],
-      zip_safe=False)
+      zip_safe=False
+)
